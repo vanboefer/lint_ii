ad75c3bf288bdb70c3dc89d434e23c8112ef24b7
jenia <jenka@proton.me>
2025-10-23 18:07:07 +0200

    feat: update the weights in the readability formula based on the new regression model

--- src/lint_ii/core/lint_scorer.py
+++ src/lint_ii/core/lint_scorer.py
@@ -9,25 +9,23 @@ class LintScorer:
         proportion_concrete: float,
     ) -> float:
         """
-        Calculate LiNT readability score using the formula.
-
-        The weights that are currently used in the formula are from the original LiNT implementation where the features were extracted with T-Scan. TO DO: redo the regression analysis and adjust the weights to fit the new implementation.
+        Calculate LiNT-II readability score using the formula.
 
         Args:
             freq_log: Average log word frequency of all content words (excluding names)
             max_sdl: Maximum syntactic dependency length
-            content_words_per_clause: Content words (excluding adverbs) per clause
+            content_words_per_clause: Content words (excluding function adverbs) per clause
             proportion_concrete: Proportion of concrete nouns out of all nouns
 
         Returns:
             LiNT score (0-100, higher = more difficult, less readable)
         """
         score = (
-            3.204
-            + (15.845 * freq_log)
-            - (1.331 * max_sdl)
-            - (3.829 * content_words_per_clause)
-            + (13.096 * proportion_concrete)
+            - 5.16
+            + (13.134 * freq_log)
+            - (1.285 * max_sdl)
+            - (3.522 * content_words_per_clause)
+            + (16.259 * proportion_concrete)
         )
         return min(100.0, max(0.0, 100 - score))
 
