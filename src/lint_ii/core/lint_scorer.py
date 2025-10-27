6bdca993dfd9642df47cded82239453a14af4304
vanboefer <38586487+vanboefer@users.noreply.github.com>
2025-10-27 11:08:29 +0100

    chore: cosmetics - coefficients dict

--- src/lint_ii/core/lint_scorer.py
+++ src/lint_ii/core/lint_scorer.py
@@ -1,6 +1,14 @@
 class LintScorer:
     """LiNT-II scoring functionality."""
 
+    COEFFICIENTS = {
+        'constant': -5.15960462,
+        'freq_log': 16.13411558,
+        'max_sdl': -1.28483912,
+        'content_words_per_clause': -3.52243722,
+        'proportion_concrete': 16.25887905,
+    }
+
     @staticmethod
     def calculate_lint_score(
         freq_log: float,
@@ -21,11 +29,11 @@ class LintScorer:
             LiNT score (0-100, higher = more difficult, less readable)
         """
         score = (
-            - 5.16
-            + (16.134 * freq_log)
-            - (1.285 * max_sdl)
-            - (3.522 * content_words_per_clause)
-            + (16.259 * proportion_concrete)
+            + LintScorer.COEFFICIENTS['constant']
+            + LintScorer.COEFFICIENTS['freq_log'] * freq_log
+            + LintScorer.COEFFICIENTS['max_sdl'] * max_sdl
+            + LintScorer.COEFFICIENTS['content_words_per_clause'] * content_words_per_clause
+            + LintScorer.COEFFICIENTS['proportion_concrete'] * proportion_concrete
         )
         return min(100.0, max(0.0, 100 - score))
 
