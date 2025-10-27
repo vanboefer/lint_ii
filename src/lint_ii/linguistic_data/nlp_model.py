a30248c2dee01211337dd5843f23588c72817730
vanboefer <38586487+vanboefer@users.noreply.github.com>
2025-10-27 13:08:01 +0100

    chore: cosmetic change to print statement

--- src/lint_ii/linguistic_data/nlp_model.py
+++ src/lint_ii/linguistic_data/nlp_model.py
@@ -5,7 +5,8 @@ from lint_ii import LiNT_II_Exception
 
 
 try:
-    print('loading Dutch language model from spacy...')
+    print('Loading Dutch language model from spaCy... ', end='')
     NLP_MODEL : Language = spacy.load('nl_core_news_lg')
+    print('✓ nl_core_news_lg')
 except OSError:
-    raise LiNT_II_Exception('LiNT-II requires the spacy model "nl_core_news_lg";download the model by running: `python -m spacy download nl_core_news_lg`')
+    raise LiNT_II_Exception('LiNT-II requires the spaCy model "nl_core_news_lg"; download the model by running: `python -m spacy download nl_core_news_lg`')
